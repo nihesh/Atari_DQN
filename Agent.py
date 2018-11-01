500967a29c1cbd167bc3cf56c15c418a55540689
nihesh <nihesh16059@iiitd.ac.in>
2018-11-02 01:42:12 +0530

    Major changes committed

--- Agent.py
+++ Agent.py
@@ -9,7 +9,7 @@ from PIL import Image
 
 class AtariAgent:
 
-	eps_greedy = -1
+	eps_greedy = 1
 	state = []
 	frame_dimension = ()
 	replay_memory_size = -1
@@ -21,22 +21,39 @@ class AtariAgent:
 	batch_size = -1
 	update_frequency = -1
 	cur_update = 1
-	min_reqd_exp = -1
 	discount_factor = -1
+	eps_dec_rate = -1
+	min_eps_greedy = -1
+	replay_start_threshold = -1
+	train_freq = -1
+	cur_train_iter = 1
+	training_epoch = []
+	episode_id = 0
+	avg_reward = 0
+	max_reward = 0
 
-	def __init__(self, action_space_size, eps_greedy, frame_dimension, replay_memory_size, input_shape, batch_size, update_frequency, min_reqd_exp, discount_factor):
+	def finishEpisode(self, score):
+
+		self.avg_reward = (self.episode_id*self.avg_reward + score)/(self.episode_id+1)
+		self.episode_id+=1
+		self.max_reward = max(self.max_reward, score)
+
+
+	def __init__(self, action_space_size, eps_greedy, frame_dimension, replay_memory_size, input_shape, batch_size, update_frequency, discount_factor, eps_dec_rate, replay_start_threshold, train_freq):
 
 		"""
 		Initialises the agent
 		"""
 
+		self.train_freq = train_freq
+		self.replay_start_threshold = replay_start_threshold
+		self.eps_dec_rate = eps_dec_rate
 		self.discount_factor = discount_factor
-		self.min_reqd_exp = min_reqd_exp
 		self.update_frequency = update_frequency
 		self.batch_size = batch_size
 		self.input_shape = input_shape
 		self.action = action_space_size
-		self.eps_greedy = eps_greedy
+		self.min_eps_greedy = eps_greedy
 		self.frame_dimension = frame_dimension
 		self.replay_memory_size = replay_memory_size
 		self.Q_Network = DQN(self.action, self.input_shape, self.batch_size, self.discount_factor)
@@ -92,17 +109,30 @@ class AtariAgent:
 		}
 
 		self.replay_memory.append(event)
+		
+		if(len(self.replay_memory)<=self.replay_start_threshold):
+			return
+
+		self.cur_update = (self.cur_update+1)%self.update_frequency
+		self.cur_train_iter = (self.cur_train_iter+1)%self.train_freq
+
+		if(self.cur_train_iter == 0):
+		
+			self.Q_Network.update_network(random.sample(self.replay_memory, self.batch_size), self.stable_predictor)
 
 		if(self.cur_update == 0):
 
-			self.Q_Network.update_network(random.sample(self.replay_memory, self.batch_size), self.stable_predictor)
 			self.stable_predictor.clf.set_weights(self.Q_Network.clf.get_weights())
 
-		self.cur_update = (self.cur_update+1)%self.update_frequency
 
 	def play(self, sample_space):
 
 		rndfloat = random.uniform(0,1)
+
+		if(len(self.replay_memory)>=self.replay_start_threshold):
+		
+			self.eps_greedy = max(self.min_eps_greedy, self.eps_greedy-self.eps_dec_rate)
+
 		if(rndfloat <= self.eps_greedy):
 			return sample_space.sample()
 
--- atari.py
+++ atari.py
@@ -4,34 +4,37 @@
 import gym
 import time
 from Agent import AtariAgent
+import pickle
 
 GAME = "Breakout-v4"
 IMG_DIM = (84, 110)
-MAX_EPISODES = 10000000
-EPS_GREEDY = 0.05
-REPLAY_MEMORY_SIZE = 40000		# Higher the better
+MAX_EPISODES = 1000000000000
+EPS_GREEDY = 0.1
+EPS_DEC_RATE = 1e-6
+REPLAY_MEMORY_SIZE = 100000		# Higher the better
+REPLAY_START = 50000
 INPUT_SHAPE = (4, 110, 84)
 BATCH_SIZE = 32
-UPDATE_FREQUENCY = 32			# Must be greater than BATCH_SIZE
-MIN_EXP_TO_UPDATE = 50			# atmost REPLAY_MEMORY_SIZE
+TRAIN_FREQUENCY = 4				# freq of steps after which training has to be done
+UPDATE_FREQUENCY = 10000		# updating predictor	
 DISCOUNT_FACTOR = 0.99
+MODEL_SAVE_RATE = 100
 
 if(__name__ == "__main__"):
 
 	environment = gym.make(GAME)
 
-	episodes = 0
+	try:
+		print("Loading existing agent")
+		file = open("./agent.obj", "rb")
+		agent = pickle.load(file)
+		file.close()
+		print("No of items in replay memory: "+str(len(agent.replay_memory)))	# Replay memory size somehow gets reset to 0 here, after restarting
+	except:	
+		print("Setting up agent")
+		agent = AtariAgent(environment.action_space.n, EPS_GREEDY, IMG_DIM, REPLAY_MEMORY_SIZE, INPUT_SHAPE, BATCH_SIZE, UPDATE_FREQUENCY, DISCOUNT_FACTOR, EPS_DEC_RATE, REPLAY_START, TRAIN_FREQUENCY)
 
-	training_epoch = []
-	cumulative_scores = []
-
-	MIN_EXP_TO_UPDATE = min(MIN_EXP_TO_UPDATE, REPLAY_MEMORY_SIZE-1)
-	agent = AtariAgent(environment.action_space.n, EPS_GREEDY, IMG_DIM, REPLAY_MEMORY_SIZE, INPUT_SHAPE, BATCH_SIZE, UPDATE_FREQUENCY, MIN_EXP_TO_UPDATE, DISCOUNT_FACTOR)
-
-	max_cum_reward = -1
-	avg = 0
-
-	while episodes < MAX_EPISODES:
+	while agent.episode_id < MAX_EPISODES:
 
 		init_state = environment.reset()
 
@@ -47,20 +50,27 @@ if(__name__ == "__main__"):
 			next_state,reward,done,_ = environment.step(action_to_perform)
 			environment.render()
 			agent.update(action_to_perform, next_state, reward, done)
-			score += reward
+			score += min(max(reward,-1),1)
 
 			# Game ended?
 			if(done):
 				break
 
-		episodes+=1
-		training_epoch.append(episodes)
-		cumulative_scores.append(score)
-		max_cum_reward = max(score, max_cum_reward)
-		avg = (avg*(len(cumulative_scores)-1) + score)/len(cumulative_scores)
+		if(agent.episode_id%MODEL_SAVE_RATE == 0):
+			file = open("./agent.obj","wb")
+			print("Serialising agent")
+			pickle.dump(agent, file)
+			file.close()
+			file = open("./agent.obj","rb")
+			agent = pickle.load(file)
+			file.close()
+			print(len(agent.replay_memory))		# Prints the actual size of replay memory 
+		
+		agent.finishEpisode(score)
 
-		print("Score obtained in Episode "+str(episodes)+" = "+str(score))
-		print("Max score so far = "+str(max_cum_reward))
-		print("Avg score so far = "+str(avg))
+		print("Score obtained in Episode "+str(agent.episode_id)+" = "+str(score))
+		print("Max score so far = "+str(agent.max_reward))
+		print("Avg score so far = "+str(agent.avg_reward))
+
 
 	environment.close()
